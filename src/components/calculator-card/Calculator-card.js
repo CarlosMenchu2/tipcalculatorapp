1248c86b11cae4ec0acac86b8fa0a457588c47c0
Carlos Menchu <carlosmenchu2@gmail.com>
2022-01-21 23:59:49 -0600

    Se agregaron los cambios finales a la calculadora

--- src/components/calculator-card/Calculator-card.js
+++ src/components/calculator-card/Calculator-card.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Calculator-Card.css';
 
@@ -21,121 +21,230 @@ import { Box } from '@mui/material';
 
 
 export const CalculatorCard = () => {
+
+    const [bill,setBill] = useState(0);
+    const [custom, setCustom] = useState(0);
+    const [numberPeople,setNumberPeople] = useState(0);
+    const [tipPercentage,setTipPercentage] = useState(0);
+    const [tipAmount,setTipAmount] = useState(0);
+    const [total,setTotal] = useState(0);
+    
+
+    /**
+     * 
+     * @param {Event} e 
+     */
+    const handleBill = (e) => {
+        setBill(e.target.value);
+    }
+
+    /**
+     * 
+     * @param {Event} e 
+     */
+    const handleCustom = (e) => {
+        setCustom(e.target.value);
+    }
+
+    /**
+     * @param {Event} e
+     */
+    const handleNumberPeople = (e) => {
+        setNumberPeople(e.target.value);
+    }
+
+    const handleTipPercentage = (tipPercentage) => {
+        setTipPercentage(tipPercentage);
+    }
+
+    useEffect(() => {
+
+        if(bill!=0 && numberPeople!=0 && tipPercentage!=0) {
+            const totalAmount = bill*tipPercentage;
+            const totalPerson = totalAmount/numberPeople;
+            setTipAmount(totalPerson);
+            setTotal(totalAmount); 
+            setCustom(0);
+        }
+        
+        if(bill!=0 && numberPeople!=0 && custom!=0) {
+            const totalAmount = bill*(custom/100);
+            const totalPerson = totalAmount/numberPeople;
+            setTipAmount(totalPerson);
+            setTotal(totalAmount); 
+            setTipPercentage(0);
+        }
+
+        if(bill==0 || bill=="") {
+            setTipAmount(0);
+            setTotal(0);
+        }
+
+        if(numberPeople==0 || numberPeople=="") {
+            setTipAmount(0);
+            setTotal(0);
+        }
+
+    }, [bill,numberPeople,custom,tipPercentage]);
+
+    const resetButtons = () => {
+        setBill(0);
+        setNumberPeople(0);
+        setTipPercentage(0);
+        setTipAmount(0);
+        setTotal(0);
+        setCustom(0);
+    }
+    
+
     return (
-        <Container maxWidth='lg' sx={{margin:'auto' ,width:'100vh', height:'100vh'}}>
-            <Grid container>
-                <Grid item xs={12} md={6}>
-                    <Stack>
-                        <FormControl sx={{mb:3}}>
-                            <InputLabel htmlFor='inputFactura'>Factura</InputLabel>
-                            <OutlinedInput
-                                id='inputFactura'
-                                startAdornment={<InputAdornment position='start'>$</InputAdornment>}
-                                label='Factura'
-                            />
-                        </FormControl>
-                        <Grid container wrap='wrap' spacing={1}>
-                            <Grid item xs={4}>
-                                <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}>
-                                    5%
-                                </Button>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}>
-                                    10%
-                                </Button>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}>
-                                    15%
-                                </Button>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}>
-                                    25%
-                                </Button>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}>
-                                    50%
-                                </Button>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <TextField id="outlined-basic" label="Custom" variant="outlined"/>
-                            </Grid>
-                        </Grid>
-                        <FormControl sx={{mt:3}}>
-                            <InputLabel htmlFor='inputFactura'>Numero de personas</InputLabel>
-                            <OutlinedInput
-                                id='inputFactura'
-                                startAdornment={<InputAdornment position='start'><PersonIcon/></InputAdornment>}
-                                label='Numero de personas'
-                            />
-                        </FormControl>
-                    </Stack>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Box m={3}>
-                        <Grid container justifyContent='space-around' alignItems='center' >
-                            <Grid item xs={6}>
-                                <Stack >
-                                    <Typography
-                                        variant='h6'
-                                        component='div'
-                                    >
-                                        Cantidad de propina
-                                    </Typography>
-                                    <Typography
+        <Container maxWidth='lg'>
+            <Grid container alignItems='center' justifyContent='center' sx={{margin:'auto',width:'100vh', height:'100vh'}}>
+                <Grid container direction='column' className='calculator-card'>
+                    <Typography
+                        textAlign='center'
+                        variant='h4'
+                        component='div'
+                        sx={{fontWeight:'bold', mb:1}}
+                    >
+                        Calculadora de Propinas
+                    </Typography>
+                    <Grid container item sx={12}>
+                        <Grid item xs={12} md={6}>
+                            <Stack>
+                                <FormControl sx={{mb:3}}>
+                                    <InputLabel htmlFor='inputFactura'>Monto de factura</InputLabel>
+                                    <OutlinedInput
+                                        value={bill}
+                                        onChange={handleBill}
+                                        id='inputFactura'
+                                        startAdornment={<InputAdornment position='start'>$</InputAdornment>}
+                                        label='Monto de factura'
+                                    />
+                                </FormControl>
+                                <Typography
                                     variant='subtitle1'
                                     component='div'
-                                    >
-                                        / persona
-                                    </Typography>
-                                </Stack>
-                            </Grid>
-                            <Grid item xs={6} container justifyContent='center'>
-                                <Typography
-                                    variant='h4'
-                                    component='div'
-
                                 >
-                                    $ 0.00
+                                    Porcentaje de propina
                                 </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid container justifyContent='space-around' alignItems='center' sx={{mt:4}}>
-                            <Grid item xs={6}>
-                                <Stack >
-                                    <Typography
-                                        variant='h6'
-                                        component='div'
-                                    >
-                                        Total
-                                    </Typography>
-                                    <Typography
-                                    variant='subtitle1'
-                                    component='div'
-                                    >
-                                        / persona
-                                    </Typography>
-                                </Stack>
-                            </Grid>
-                            <Grid item container xs={6} justifyContent='center'>
-                                <Typography
-                                    variant='h4'
-                                    component='div'
+                                <Grid container wrap='wrap' spacing={1}>
+                                    <Grid item xs={4}>
+                                        <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}
+                                            onClick={()=>handleTipPercentage(0.05)}
+                                        >
+                                            5%
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}
+                                            onClick={()=> handleTipPercentage(0.10)}
+                                        >
+                                            10%
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}
+                                            onClick={()=>handleTipPercentage(0.15)}
 
-                                >
-                                    $ 0.00
-                                </Typography>
-                            </Grid>
+                                        >
+                                            15%
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}
+                                            onClick={()=>handleTipPercentage(0.25)}
+                                        >
+                                            25%
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Button variant="contained" disableElevation className='percentage-button' sx={{fontSize:'1.4rem'}}
+                                            onClick={()=>handleTipPercentage(0.50)}
+                                        >
+                                            50%
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <TextField id="outlined-basic" label="Otro" variant="outlined" type='number' value={custom} onChange={handleCustom}/>
+                                    </Grid>
+                                </Grid>
+                                <FormControl sx={{mt:3}}>
+                                    <InputLabel htmlFor='inputFactura'>Numero de personas</InputLabel>
+                                    <OutlinedInput
+                                        value={numberPeople}
+                                        onChange={handleNumberPeople}
+                                        id='inputFactura'
+                                        startAdornment={<InputAdornment position='start'><PersonIcon/></InputAdornment>}
+                                        label='Numero de personas'
+                                    />
+                                </FormControl>
+                            </Stack>
                         </Grid>
-                        <Grid container sx={{mt:4}}>
-                            <Grid item container xs={12} justifyContent='center'>
-                                <Button variant="contained" disableElevation sx={{width:'100%', maxWidth:'400px'}}> RESET </Button>
-                            </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Box m={3}>
+                                <Grid container justifyContent='space-around' alignItems='center' >
+                                    <Grid item xs={6}>
+                                        <Stack >
+                                            <Typography
+                                                variant='h6'
+                                                component='div'
+                                            >
+                                                Cantidad de propina
+                                            </Typography>
+                                            <Typography
+                                            variant='subtitle1'
+                                            component='div'
+                                            >
+                                                / persona
+                                            </Typography>
+                                        </Stack>
+                                    </Grid>
+                                    <Grid item xs={6} container justifyContent='center'>
+                                        <Typography
+                                            variant='h4'
+                                            component='div'
+
+                                        >
+                                            $ {tipAmount.toFixed(2)}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Grid container justifyContent='space-around' alignItems='center' sx={{mt:4}}>
+                                    <Grid item xs={6}>
+                                        <Stack >
+                                            <Typography
+                                                variant='h6'
+                                                component='div'
+                                            >
+                                                Total
+                                            </Typography>
+                                            <Typography
+                                            variant='subtitle1'
+                                            component='div'
+                                            >
+                                                / persona
+                                            </Typography>
+                                        </Stack>
+                                    </Grid>
+                                    <Grid item container xs={6} justifyContent='center'>
+                                        <Typography
+                                            variant='h4'
+                                            component='div'
+
+                                        >
+                                            $ {total.toFixed(2)}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Grid container sx={{mt:5}}>
+                                    <Grid item container xs={12} justifyContent='center'>
+                                        <Button fullWidth variant="contained" disableElevation onClick={resetButtons}> RESET </Button>
+                                    </Grid>
+                                </Grid>
+                            </Box>
                         </Grid>
-                    </Box>
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
